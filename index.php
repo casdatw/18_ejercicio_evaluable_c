1851ff4539a0ca685e1ad9b4df094d50aeffd79d
casdatw <casdatw@gmail.com>
2025-02-24 12:55:01 +0100

    background-color

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            background-color: blue;
+        }
+        
+    </style>
+</head>
+<body>
+    
+
 <?php
 $curso = "Desarrollo de aplicaciones con tecnologia web";
 
@@ -11,3 +27,5 @@ function datosCurso(){
 }
 datosCurso();
 ?>
+</body>
+</html>
